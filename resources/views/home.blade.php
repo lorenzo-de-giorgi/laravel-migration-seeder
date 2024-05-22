b93ecf9a97e8e4890ddbdd8d93992681ced76873
Degio <80683176+lorenzo-de-giorgi@users.noreply.github.com>
2024-05-22 14:29:38 +0200

    Initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('title', 'Home')
+
+@section('content')
+    <div class="ldg-main">
+        <section class="container text-center">
+            Body
+        </section>
+    </div>
+@endsection
